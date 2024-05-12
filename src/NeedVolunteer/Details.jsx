692aa970dab6f96228a41d3fd72b6574bc8ff991
Sangram Bishwas <info.sangram2003@gmail.com>
2024-05-12 17:39:24 +0600

    adde functionality in the button of request in post details pages

--- src/NeedVolunteer/Details.jsx
+++ src/NeedVolunteer/Details.jsx
@@ -67,7 +67,7 @@ const Details = () => {
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrpition</label>
                             <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={descrption} readOnly />
                         </div>
-                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Request</button>
+                        <button onClick={handleRequest} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Request</button>
                     </form>
                 </div>
             </dialog>
