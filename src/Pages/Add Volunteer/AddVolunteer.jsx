31baf57292f500409803145ab4ee32e11759a3f9
Sangram Bishwas <info.sangram2003@gmail.com>
2024-05-13 11:33:14 +0600

    Post method has been implement in add voluteer page and some modification on need volunteer page

--- src/Pages/Add Volunteer/AddVolunteer.jsx
+++ src/Pages/Add Volunteer/AddVolunteer.jsx
@@ -1,14 +1,48 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
+import useAuth from "../../CustomHooks/useAuth";
+import useAxios from "../../CustomHooks/useAxios";
+import toast from "react-hot-toast";
 const AddVolunteer = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxios()
     const [startDate, setStartDate] = useState(new Date());
+    const handleAddVolunteer = (e) => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+
+        const email = user?.email;
+        const userName = user?.displayName;
+        const category = form.get('category');
+        const image = form.get('image');
+        const description = form.get('description');
+        const numberOfVolunteer = form.get('numberOfVolunteers');
+        const location = form.get('location');
+        const deadline = form.get('deadline');
+        const title = form.get('title');
+
+        const newPost = { title, image, category, description, userName, email, location, numberOfVolunteer, deadline };
+        console.log(newPost);
+        const url = '/posts'
+
+        axiosSecure.post(url, newPost)
+            .then(res => {
+                const data = res.data;
+                console.log(data);
+                if (data.acknowledged) {
+                    toast('New post added successfully')
+                }
+            })
+    }
+    
     return (
         <div className="my-10">
             <h2 className="text-center font-bold text-xl md:text-3xl">Add Your Post</h2>
-            <form className="max-w-md mx-auto my-5">
+            <form onSubmit={handleAddVolunteer} className="max-w-md mx-auto my-5">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="email" name="title" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" name="title" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Post Title</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
@@ -21,7 +55,7 @@ const AddVolunteer = () => {
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 w-full mb-5 group">
-                        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="text" name="name" defaultValue={user?.displayName} id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " readOnly />
                         <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
@@ -49,12 +83,12 @@ const AddVolunteer = () => {
                     </div>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="email" name="email" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input type="email" name="email" defaultValue={user?.email} id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " readOnly />
                     <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
                 </div>
                 <div className="mb-5 w-full">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrpition</label>
-                    <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <input type="text" name="description" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">POST</button>
             </form>
