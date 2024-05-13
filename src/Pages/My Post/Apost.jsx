44c2d04ef58173a179357774ad39ce7268cc7db9
Sangram Bishwas <info.sangram2003@gmail.com>
2024-05-14 01:48:45 +0600

    implement update method with sweet alert in my posts page

--- src/Pages/My Post/Apost.jsx
+++ src/Pages/My Post/Apost.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
 import useAxios from '../../CustomHooks/useAxios';
+import useAuth from '../../CustomHooks/useAuth';
+import DatePicker from "react-datepicker";
+import { useState } from 'react';
 
 const Apost = ({ post, posts, setPosts }) => {
-    const { _id, category, title, image } = post;
+    const { user } = useAuth();
+    const { _id, category, title, image, description, location, numberOfVolunteer } = post;
     const axiosSecure = useAxios();
+    const [startDate, setStartDate] = useState(new Date());
 
     const handleDelete = (id) => {
         console.log('delete this craft', id);
@@ -39,6 +44,39 @@ const Apost = ({ post, posts, setPosts }) => {
             }
         });
     }
+
+    const handleUpdate = (e) => {
+
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+
+        const category = form.get('category');
+        const image = form.get('image');
+        const description = form.get('description');
+        const numberOfVolunteer = form.get('numberOfVolunteers');
+        const location = form.get('location');
+        const deadline = form.get('deadline');
+        const title = form.get('title');
+
+        const updatePost = { title, image, category, description, location, numberOfVolunteer, deadline };
+        console.log(updatePost);
+
+        axiosSecure.put(`/posts/${_id}`, updatePost)
+            .then(res => {
+                const data = res.data;
+                console.log(data)
+                if (data.modifiedCount > 0) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Craft Updated Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    })
+                }
+
+            })
+       
+    }
     return (
         <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
             <div className="w-full h-64 bg-black bg-center bg-cover rounded-lg shadow-md" style={{ backgroundImage: `url("${image}")` }}>
@@ -52,9 +90,62 @@ const Apost = ({ post, posts, setPosts }) => {
 
                 <div className="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
                     <button onClick={() => handleDelete(_id)} className="btn btn-sm px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-red-700 rounded hover:bg-red-500 dark:hover:bg-red-600 focus:bg-red-700 dark:focus:bg-red-600 focus:outline-none">Delete</button>
-                    <button className="btn btn-sm px-2 py-1 text-xs     font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Update</button>
+                    <button onClick={() => document.getElementById('my_modal_5').showModal()} className="btn btn-sm px-2 py-1 text-xs     font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Update</button>
                 </div>
             </div>
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h2 className="text-3xl font-bold text-center">Update Your Post</h2>
+                    <form onSubmit={handleUpdate} method="dialog" className="max-w-md mx-auto my-5">
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="title" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={title} />
+                            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Post Title</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="category" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={category} />
+                            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Category</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="image" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={image} />
+                            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Photo URL</label>
+                        </div>
+                        <div className="grid md:grid-cols-2 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="number" name="numberOfVolunteers" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={numberOfVolunteer} />
+                                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Number of volunteers</label>
+                            </div>
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="location" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={location} />
+                                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Location</label>
+                            </div>
+                        </div>
+                        <div className="grid md:grid-cols-2 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={user?.displayName} readOnly />
+                                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+                            </div>
+                            <div className="relative z-0 w-full my-2.5 group">
+                                <DatePicker
+                                    className="mr-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    name="deadline"
+                                    showIcon
+                                    selected={startDate}
+                                    onChange={(date) => setStartDate(date)}
+                                    placeholderText=" " />
+                            </div>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="email" name="email" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " defaultValue={user?.email} readOnly />
+                            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+                        </div>
+                        <div className="mb-5 w-full">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrpition</label>
+                            <input type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={description} />
+                        </div>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+                    </form>
+                </div>
+            </dialog>
         </div>
     );
 };
